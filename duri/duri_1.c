54ac45ec5321dead085e53856df473e44ce0ad7d
신우진 <swj990302@gmail.com>
2022-06-17 00:23:34 +0900

    new class

---
+++ duri/duri_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//정해진 길이를 이용 넓이를 구하는 구문
+
+int main()
+{
+	int width, height, area;
+
+	width = 10;
+	height = 5;
+	area = width * height;
+
+	printf ("면적 = %d", area);
+
+	return 0;
+}
---
+++ duri/duri_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int summer, winter, sum;
+
+	summer = 28;
+	winter = 38;
+
+	sum = winter + summer;
+	
+	printf ("합계 : %d일", sum);
+
+	return 0;
+}
